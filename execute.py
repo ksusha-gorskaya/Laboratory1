144e9a17373eff7264419059ee22f70056671e3e
ksusha-gorskaya <ksusha-gorskaya@mail.ru>
2018-02-02 00:25:38 +0300

    Executer @Cadovvl

--- execute.py
+++ execute.py
@@ -1,10 +1,52 @@
-
 def parse_token(token):
-    # ???
+    token = token.split()
     return token
 
-OPERATOR_TO_LAMBDA = {
-}
+def number_is_int(a1,a2):
+    return (not '.' in str(a1) and not '.' in str(a2))
 
 def execute(line):
-    return 0.0
+    line = parse_token(line)
+    print(line)
+    id=0
+    arr=[]
+    tmpRes="empty"
+    for item in line:
+        if id>=2:
+            if (id == 2):
+                arr.append(float(line[id - 2])) if '.' in line[id-2] else arr.append(int(line[id-2]))
+                arr.append(float(line[id - 1])) if '.' in line[id - 1] else arr.append(int(line[id - 1]))
+            if item=='+':
+                a1 = arr.pop()
+                a2 = arr.pop()
+                if number_is_int(a1, a2):
+                    tmpRes = int(a2) + int(a1)
+                else:
+                    tmpRes = float(a2) + float(a1)
+            if item=='-':
+                a1 = arr.pop()
+                a2 = arr.pop()
+                if number_is_int(a1, a2):
+                    tmpRes = int(a2) - int(a1)
+                else:
+                    tmpRes = float(a2) - float(a1)
+            if item=='*':
+                a1 = arr.pop()
+                a2 = arr.pop()
+                if number_is_int(a1, a2):
+                    tmpRes = int(a2) * int(a1)
+                else:
+                    tmpRes = float(a2) * float(a1)
+            if item=='/':
+                a1 = arr.pop()
+                a2 = arr.pop()
+                if number_is_int(a1, a2):
+                    tmpRes = int(int(a2) / int(a1))
+                else:
+                    tmpRes = float(a2) / float(a1)
+            arr.append(item) if tmpRes=="empty" else arr.append(tmpRes)
+            tmpRes="empty"
+        id+=1
+    if id == 2:
+        return "Error. Incorrect input line."
+    return arr.pop()
